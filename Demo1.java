895b2bfae48c68eb12804b62d59a4976c850f514
sandeep1903-arch <prasandeep822@gmail.com>
2026-03-24 15:32:55 +0530

    add

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,7 @@
-class Demo {
+public class Demo1 {
     public static void main(String[] args)
     {
         System.out.println("Hello Sandeep");
+        System.out.println("How are you");
     }
 }
